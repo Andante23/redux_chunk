c7d86b2ee864aa4278dae43da9677a749392cb61
andan <andante1104@naver.com>
2024-02-21 15:36:27 +0900

    axios json-server를 이용하였을 때 코드 바꾸는 중.....

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 
 function Profile() {
   const { avatar, nickname, userId } = useSelector((state) => state.letter);
+
+  // 수정모드 비활성화 활성화
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [editedContent, setEditedContent] = useState();
+  // const [uploadUrl , setUploadUrl] = useState("")
+
+  // const onClickProfileUpdateButtonClick = async (event) => {
+
+  //   await axios.patch('http://localhost:5000/letters',
+  //        {
+  //          "avatar" : uploadUrl
+  //          "nickname" : editedContent
+  //        },
+  //        {
+  //         "Authorization":"Bearer AccessToken"
+  //        }
+
+  //   )
+  // }
+
+  // const handleEdit = () => setIsEditing(true);
 
   return (
     <>
@@ -11,7 +34,7 @@ function Profile() {
           <StProfileImg src={avatar} alt="" /> <br />
           <b>{nickname}</b>
           <p>{userId}</p>
-          <button>수정하기</button>
+          <button onClick={id}>수정하기</button>
         </div>
       }
     </>
